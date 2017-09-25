0c1d49401036ac883be0731e55805e6d04ab4a31
dhcodes <thor.dh@gmail.com>
2017-09-25 15:23:56 -0500

    routes and controllers

--- locallib/app.js
+++ locallib/app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
+const catalog = require('./routes/catalog');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/wiki', wiki);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
