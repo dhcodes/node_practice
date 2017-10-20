4160346bc7c5d5d6cef89eca59423723c67fad4d
dhcodes <thor.dh@gmail.com>
2017-10-20 09:46:56 -0500

    more controller and view work

--- locallib/controllers/bookController.js
+++ locallib/controllers/bookController.js
@@ -5,7 +5,7 @@ const BookInstance = require('../models/bookinstance');
 
 const async = require('async');
 
-exports.index = function(req, res) {
+exports.index = function(req, res, next) {
   async.parallel({
     book_count: function(callback) {
       Book.count(callback);
@@ -41,8 +41,25 @@ exports.book_list = function(req, res, next) {
 };
 
 // Display detail page for a specific book
-exports.book_detail = function(req, res) {
-  res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+exports.book_detail = function(req, res, next) {
+  async.parallel({
+    book: function(callback) {       
+      Book.findById(req.params.id)
+        .populate('author')
+        .populate('genre')
+        .exec(callback);
+    },
+    book_instance: function(callback) {
+
+      BookInstance.find({ 'book': req.params.id })
+        //.populate('book')
+        .exec(callback);
+    },
+  }, function(err, results) {
+    if (err) { return next(err); }
+    //Successful, so render
+    res.render('book_detail', { title: 'Title', book: results.book, book_instances: results.book_instance });
+  });
 };
 
 // Display book create form on GET
