4963d68ff188c318e35d765ff436b010edda22af
dhcodes <thor.dh@gmail.com>
2017-11-05 14:33:46 -0600

    genre post controller

--- locallib/app.js
+++ locallib/app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -27,6 +28,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- locallib/controllers/genreController.js
+++ locallib/controllers/genreController.js
@@ -35,13 +35,55 @@ exports.genre_detail = function(req, res, next) {
 };
 
 // Display Genre create form on GET
-exports.genre_create_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Genre create GET');
+exports.genre_create_get = function(req, res, next) {
+  res.render('genre_form', { title: 'Create Genre' });
 };
 
 // Handle Genre create on POST
-exports.genre_create_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: Genre create POST');
+exports.genre_create_post = function(req, res, next) {
+
+  //Check that name field is not empty
+  req.checkBody('name', 'Genre name required').notEmpty();
+  
+  //Trim and escape the name field.
+  req.sanitize('name').escape();
+  req.sanitize('name').trim();
+
+  //Run validators
+  let errors = req.validationErrors();
+
+  //Create a genre object with escaped and trimmed data.
+  let genre = new Genre(
+    { name: req.body.name }
+  );
+  
+  if (errors) {
+    //If there are errors render the form again, with prev values and errors
+    res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors });
+    return;
+  }
+  else {
+    //Data from form is valid.
+    // Check if Genre with same name already exists
+    Genre.findOne({ 'name': req.body.name })
+      .exec( function(err, found_genre ) {
+        console.log('found_genre: ' + found_genre );
+        if (err) { return next(err); }
+
+        if (found_genre) {
+          res.redirect(found_genre.url);
+        }
+        else {
+          genre.save(function (err) {
+            if (err) { return next(err); }
+            //saved Genre. Redirect to genre detail.
+            res.redirect(genre.url);
+          });
+
+        }
+      });
+  }
+  
 };
 
 // Display Genre delete form on GET
